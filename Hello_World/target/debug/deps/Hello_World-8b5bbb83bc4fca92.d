d254ca461162ec8544d5e89c3db7c02f484fa90a
Atharv <atharvpathak248@gmail.com>
2024-04-09 12:51:31 +0530

    deleting project

--- Hello_World/target/debug/deps/Hello_World-8b5bbb83bc4fca92.d
+++
@@ -1,5 +0,0 @@
-C:\Users\athar\Desktop\timepass\rust\Hello_World\target\debug\deps\libHello_World-8b5bbb83bc4fca92.rmeta: src\main.rs
-
-C:\Users\athar\Desktop\timepass\rust\Hello_World\target\debug\deps\Hello_World-8b5bbb83bc4fca92.d: src\main.rs
-
-src\main.rs:
--- Hello_World/target/debug/deps/Hello_World-ed0f2268b3cb4d14.d
+++
@@ -1,5 +0,0 @@
-C:\Users\athar\Desktop\timepass\rust\Hello_World\target\debug\deps\libHello_World-ed0f2268b3cb4d14.rmeta: src\main.rs
-
-C:\Users\athar\Desktop\timepass\rust\Hello_World\target\debug\deps\Hello_World-ed0f2268b3cb4d14.d: src\main.rs
-
-src\main.rs:
