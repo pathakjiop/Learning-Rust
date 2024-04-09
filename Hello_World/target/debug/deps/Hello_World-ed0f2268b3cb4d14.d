d3060f0017941e5b5951fb2947c7611909f1e5cd
Atharv <atharvpathak248@gmail.com>
2024-04-09 12:39:14 +0530

    Hello World

---
+++ Hello_World/target/debug/deps/Hello_World-ed0f2268b3cb4d14.d
@@ -0,0 +1,5 @@
+C:\Users\athar\Desktop\timepass\rust\Hello_World\target\debug\deps\libHello_World-ed0f2268b3cb4d14.rmeta: src\main.rs
+
+C:\Users\athar\Desktop\timepass\rust\Hello_World\target\debug\deps\Hello_World-ed0f2268b3cb4d14.d: src\main.rs
+
+src\main.rs:
